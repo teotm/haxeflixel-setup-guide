71da935eeeb26475d9a8bf5de61d690cb876e6d1
teotm <84466597+teotm@users.noreply.github.com>
2022-06-06 23:03:20 +0200

    Update 2_haxe_commands_part_2.bat

--- batchFiles/2_haxe_commands_part_2.bat
+++ batchFiles/2_haxe_commands_part_2.bat
@@ -4,19 +4,25 @@ title Haxe gaming
 goto haxeupdate
 
 :haxeupdate
-haxelib install flixel-addons
 haxelib install flixel-ui
-haxelib install hscript
-haxelib install newgrounds
-haxelib remove linc_luajit
-haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit.git
-haxelib remove hxvm-luajit
+haxelib install flixel-addons
+haxelib install tjson
+haxelib install hxjsonast
+haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
 haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
-haxelib git faxe https://github.com/uhrobots/faxe
-haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib install hscript 
+haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib install hxcpp-debug-server
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib install newgrounds
 haxelib install actuate
-haxelib remove extension-webm
 haxelib git extension-webm https://github.com/KadeDev/extension-webm
 lime rebuild extension-webm windows
+haxelib git tentools https://github.com/TentaRJ/tentools.git
+haxelib git systools https://github.com/haya3218/systools
+haxelib run lime rebuild systools windows
+haxelib git polymod https://github.com/MasterEric/polymod.git
+haxelib install hxCodec 2.5.1
+haxelib list
 pause
